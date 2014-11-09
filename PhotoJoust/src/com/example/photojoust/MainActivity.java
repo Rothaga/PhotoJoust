69cd37d661b280b9bd48852679f624b65b3c6e6b
askem- <lope058@usc.edu>
2014-11-08 18:18:13 -0800

    added ability for users to login

--- PhotoJoust/src/com/example/photojoust/MainActivity.java
+++ PhotoJoust/src/com/example/photojoust/MainActivity.java
@@ -1,21 +1,45 @@
 package com.example.photojoust;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+	Firebase ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
+        ref = new Firebase("https://photojoust.firebaseio.com");
         
+        
+    }
+    
+    public void loginUser(View view){
+    	System.out.println("hello");
+    	EditText editText = (EditText) findViewById(R.id.editText1);
+    	//System.out.println(editText.getText().toString());
+    	EditText editText2 = (EditText) findViewById(R.id.editText2);
+    	//System.out.println(editText2.getText().toString());
+    	ref.authWithPassword(editText.getText().toString(), editText2.getText().toString(), new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+            }
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                // there was an error
+            }
+        });
     }
 
 
