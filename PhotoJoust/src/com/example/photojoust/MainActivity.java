0da66e88b0a1b828ed68eb90f63b8b46b763d017
askem- <lope058@usc.edu>
2014-11-08 17:22:31 -0800

    -did some stuff. added firebase

--- PhotoJoust/src/com/example/photojoust/MainActivity.java
+++ PhotoJoust/src/com/example/photojoust/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.photojoust;
 
+import com.firebase.client.Firebase;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +14,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        
     }
 
 
